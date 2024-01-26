3b7555bd69e7e87670845ce63adc113af25067bf
TruongNguyen07122003 <96875408+TruongNguyen07122003@users.noreply.github.com>
2024-01-27 03:27:24 +0700

    homepage

---
+++ WebPage/WebBlog/WebBlog.js
@@ -0,0 +1,9 @@
+
+  
+    $(".single-carousel").slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: true,
+      prevArrow: '<button class="slick-prev"><i class=" fa fa-angle-left"></i></button>',
+    nextArrow: '<button class="slick-next"><i class="fa fa-angle-right"></i></button>',
+    });
