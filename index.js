0c78d78978dcf1e82e2562bfb0e371b2fbc8f9c2
TruongNguyen07122003 <96875408+TruongNguyen07122003@users.noreply.github.com>
2024-01-30 02:13:53 +0700

    delete login of Tung and put it into index page , appear after click quote button

--- index.js
+++ index.js
@@ -14,4 +14,34 @@ $(document).ready(function() {
         prevArrow: '<button class="slick-prev"><i class=" fa fa-angle-left"></i></button>',
         nextArrow: '<button class="slick-next"><i class="fa fa-angle-right"></i></button>',
     });
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Lấy tham chiếu đến nút "Quote"
+    var quoteButton = document.getElementById("quoteButton");
+    var overlay = document.getElementById("overlay");
+    // Lấy tham chiếu đến thẻ chứa form login
+    var loginCard = document.getElementById("loginCard");
+
+    // Bắt sự kiện click vào nút "Quote"
+    quoteButton.addEventListener("click", function() {
+        // Hiển thị form login
+        loginCard.style.display = "block";
+        overlay.style.zIndex = "100";
+        overlay.style.backgroundColor="#DFE9F4";
+        overlay.style.opacity="0.5";
+    });
+    document.addEventListener("DOMContentLoaded", function() {
+        var quoteButton = document.getElementById("quoteButton");
+        var loginCard = document.getElementById("loginCard");
+    
+        quoteButton.addEventListener("click", function() {
+            loginCard.style.opacity = "1"; // Hiển thị form login
+        });
+    });
+    
+
+    
+    
 });
