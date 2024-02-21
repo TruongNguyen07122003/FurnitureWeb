0b6718cd76bebd84378605a1899d497034f37164
TruongNguyen07122003 <96875408+TruongNguyen07122003@users.noreply.github.com>
2024-02-22 00:46:52 +0700

    feat: profile for team member in about us page

--- WebPage/AboutUsPage/ABoutUsPage.js
+++ WebPage/AboutUsPage/ABoutUsPage.js
@@ -30,8 +30,40 @@ $(document).ready(function() {
         ]
     });
 
+        
     
+
+
+});
+$(document).ready(function() {
+    $(".staff-carousel").slick({
+        centerMode: true,
+        centerPadding: '20px',
+        arrows: true,
+        prevArrow: '<button class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+        nextArrow: '<button class="slick-next"><i class="fa fa-angle-right"></i></button>',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '20px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '10px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
 
-    
-
+ 
